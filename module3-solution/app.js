1e551a9386083d59f7ac385552d4544e923fcb3b
Sharma <vijay-sai.sharma@capgemini.com>
2024-05-15 21:30:04 +0530

    initial implementation

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,3 +1,30 @@
 (function(){
 
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .constant('ApiBasePath', 'https://coursera-jhu-default-rtdb.firebaseio.com');
+
+        NarrowItDownController.$inject['MenuSearchService'];
+
+    function NarrowItDownController(MenuSearchService){
+        narrowItDown = this;
+    };
+
+    MenuSearchService.$inject['$http', 'ApiBasePath'];
+
+    function MenuSearchService($http, ApiBasePath){
+        service = this;
+
+        service.getMatchedMenuItems = function(searchTerm){
+            return $http().then(function (result) {
+                // process result and only keep items that match
+                var foundItems;
+            
+                // return processed items
+                return foundItems;
+            });
+        };
+    };
+
 })();
