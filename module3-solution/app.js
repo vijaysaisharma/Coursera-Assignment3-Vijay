fa557506bdc9b563608466c85142d85b01588910
Sharma <vijay-sai.sharma@capgemini.com>
2024-05-16 20:14:58 +0530

    updated description searchTerm

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -30,7 +30,7 @@
 
         var promise = MenuSearchService.getMatchedMenuItems("tofu");
         promise.then(function(response){
-            console.log('response: ', response); 
+            //console.log('response: ', response.A.menu_items); 
         });
     }
 
@@ -46,8 +46,29 @@
             }).then(function (result) {
                 // process result and only keep items that match
                 var foundItems;
+
+                var resultArray = [];
+                var indexArray = [];
+                var dataArray = Array.from(Object.values(result.data));
                 
+                dataArray.forEach(function(value, index){
+                    var currentMenuItem = value.menu_items;
+                    
+                    currentMenuItem.forEach(function(menuItem, itemIndex){
+                        if(menuItem.description.includes(searchTerm)){
+                        resultArray.push({
+                            "description": menuItem.description,
+                            "name": menuItem.name,
+                            "short_name": menuItem.short_name
+                        });
+                    };
+                    });
+                });
+
+                console.log('resultArray: ', resultArray);
+
                 // return processed items
+                console.log('result.data: ', Array.from(Object.values(result.data)));
                 return result.data;
             });
         };
